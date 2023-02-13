3f9cba44a1d59be8222713cd369864e2a33932bb
AnonymousDaddyIsHere <117323662+AnonymousDaddyIsHere@users.noreply.github.com>
2023-02-13 23:05:20 +0000

    Create dl-stuff.sh

---
+++ tools/dl-stuff.sh
@@ -0,0 +1,5 @@
+echo "Burp.Suite.Professional.2022.8.5 ..............................................."
+wget https://dl2.soft98.ir/soft/b/Burp.Suite.Professional.2022.8.5.rar
+echo "Burp.Suite.Professional.2022.8.5 #################################.............."
+unrar x -p{soft98.ir} Burp.Suite.Professional.2022.8.5.rar
+echo "Burp.Suite.Professional.2022.8.5 ###############################################"
