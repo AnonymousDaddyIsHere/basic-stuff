f89566fa34c05faf359efe8bf1dd4994930623f9
AnonymousDaddyIsHere <117323662+AnonymousDaddyIsHere@users.noreply.github.com>
2023-07-19 11:38:47 +0330

    Update dl-stuff.sh

--- tools/dl-stuff.sh
+++ tools/dl-stuff.sh
@@ -1,3 +1,6 @@
+#!/bin/sh
+echo "Run me as root"
+
 echo "Burp.Suite.Professional.2022.8.5 ..............................................."
 wget https://dl2.soft98.ir/soft/b/Burp.Suite.Professional.2022.8.5.rar
 echo "Burp.Suite.Professional.2022.8.5 #################################.............."
