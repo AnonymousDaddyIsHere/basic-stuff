ad0a020526e8d4c7cd92d32a1aa2cecf104f694a
AnonymousDaddyIsHere <117323662+AnonymousDaddyIsHere@users.noreply.github.com>
2024-05-12 09:54:57 -0400

    Update dl-stuff.sh

--- tools/dl-stuff.sh
+++ tools/dl-stuff.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Run me as root"
 
-echo "Burp.Suite.Professional.2022.8.5 ..............................................."
-wget https://dl2.soft98.ir/soft/b/Burp.Suite.Professional.2022.8.5.rar
+echo "Burp.Suite.Professional.2024.3.1.4.rar ..............................................."
+wget https://dl2.soft98.ir/soft/b/Burp.Suite.Professional.2024.3.1.4.rar
 echo "Burp.Suite.Professional.2022.8.5 #################################.............."
-unrar x -p{soft98.ir} Burp.Suite.Professional.2022.8.5.rar
-echo "Burp.Suite.Professional.2022.8.5 ###############################################"
+unrar x -p{soft98.ir} Burp.Suite.Professional.2024.3.1.4.rar
+echo "Burp.Suite.Professional.2024.3.1.4.rar ###############################################"
